5b9f41869286c19e1548031f543e73393784935e
matheusfmf <matheusfreitas1402@gmail.com>
2025-07-28 17:56:40 -0300

    Refactor GitHub profile management tests for improved structure and readability

--- web/cypress/e2e/github.cy.js
+++ web/cypress/e2e/github.cy.js
@@ -3,13 +3,50 @@ describe('Gerenciamento de Perfis no GitHub', () => {
     beforeEach(() => {
         cy.login();
         cy.goTo('Tabela', 'Perfis do GitHub');
-    })
+    });
 
     it('Deve poder cadastrar um novo perfil do gitHub', () => {
-        cy.log('TO do')
-
         cy.get('#name').type('Matheus Freitas');
         cy.get('#username').type('matheusfmf');
+        cy.get('#profile').type('QA');
 
+        cy.contains('button', 'Adicionar Perfil').click();
+
+        cy.contains('table tbody tr', 'matheusfmf')
+            .should('be.visible')
+            .as('trProfile');
+
+        cy.get('@trProfile')
+            .contains('td', 'Matheus Freitas')
+            .should('be.visible');
+
+        cy.get('@trProfile')
+            .contains('td', 'QA')
+            .should('be.visible');
+
+    });
+
+    it.only('Deve poder remover um perfil do gitHub', () => {
+
+        const profile = {
+            name: 'Matheus Freitas',
+            username: 'matheus123',
+            desc: 'QA'
+        }
+        cy.get('#name').type(profile.name);
+        cy.get('#username').type(profile.username);
+        cy.get('#profile').type(profile.desc);
+
+        cy.contains('button', 'Adicionar Perfil').click();
+
+        cy.contains('table tbody tr', profile.username)
+            .should('be.visible')
+            .as('trProfile');
+
+        cy.get('@trProfile')
+            .find('button[title="Remover perfil"]').click();
+
+        cy.contains('table tbody tr', profile.username)
+            .should('not.exist');
     });
 });
